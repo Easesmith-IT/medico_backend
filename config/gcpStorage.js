46f1b3d2dfea1af1c95c046c49b76077359ddd81
mansidev03 <mansidev04@easesmith.com>
2026-03-23 13:42:23 +0530

    added json

--- config/gcpStorage.js
+++ config/gcpStorage.js
@@ -11,13 +11,22 @@
 
 // module.exports = bucket;
 
+// const { Storage } = require("@google-cloud/storage");
+// const path = require("path");
+
+// const storage = new Storage({
+//   projectId: "extreme-ability-475608-c9", // from gcpbucket.json
+//   keyFilename: path.join(__dirname, "gcpbucket.json"),
+// });
 const { Storage } = require("@google-cloud/storage");
 const path = require("path");
 
 const storage = new Storage({
-  projectId: "extreme-ability-475608-c9", // from gcpbucket.json
-  keyFilename: path.join(__dirname, "gcpbucket.json"),
+  // credentials: JSON.parse(process.env.GCP_SERVICE_ACCOUNT),
+  projectId: process.env.GCP_PROJECT_ID,
 });
+
+
 
 const bucketName = "medico_health_tech";
 
