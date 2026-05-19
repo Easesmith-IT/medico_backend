c717a8a07d680603a8b0d9c270a6c5e6b5954f66
mansidev03 <mansidev04@easesmith.com>
2026-05-19 11:41:54 +0530

    added review

--- route/reviewRoute.js
+++ route/reviewRoute.js
@@ -4,10 +4,10 @@ const reviewController = require("../controller/reviewController");
 
 const router = express.Router();
 
-router.post("/createReview", protect("patient"), reviewController.createReview);
+router.post("/createReview", protect("patient","serviceprovider"), reviewController.createReview);
 router.get("/listReviews", reviewController.listReviews);
-router.patch("/:id", protect("patient"), reviewController.updateReview);
-router.delete("/:id", protect("patient"), reviewController.deleteReview);
+router.patch("/updateReview/:id", protect("patient"), reviewController.updateReview);
+router.delete("/deleteReview:id", protect("patient"), reviewController.deleteReview);
 router.patch("/admin/:id/moderation", protect("admin", "superadmin", "subadmin"), reviewController.moderateReview);
 
 module.exports = router;
