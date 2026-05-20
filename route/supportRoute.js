a88d4d4ba402222cef02870f14d34fc60435a13e
mansidev03 <mansidev04@easesmith.com>
2026-05-20 13:47:47 +0530

    added tickets

--- route/supportRoute.js
+++ route/supportRoute.js
@@ -5,7 +5,7 @@ const supportController = require("../controller/supportController");
 const router = express.Router();
 
 router.post("/tickets", protect("patient", "doctor", "admin", "superadmin", "subadmin", "serviceprovider"), supportController.createTicket);
-router.get("/tickets/me", protect("patient", "doctor", "admin", "superadmin", "subadmin", "serviceprovider"), supportController.getMyTickets);
-router.post("/tickets/:id/messages", protect("patient", "doctor", "admin", "superadmin", "subadmin", "serviceprovider"), supportController.addMessage);
+router.get("/getMyTickets", protect("patient", "doctor", "admin", "superadmin", "subadmin", "serviceprovider"), supportController.getMyTickets);
+router.post("/addMessageTickets/:id", protect("patient", "doctor", "admin", "superadmin", "subadmin", "serviceprovider"), supportController.addMessage);
 
 module.exports = router;
