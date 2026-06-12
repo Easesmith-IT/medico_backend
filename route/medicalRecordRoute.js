65dc9a85f03b00732c0f005f3db3201b19f0d5cc
mansidev03 <mansidev04@easesmith.com>
2026-06-12 13:57:18 +0530

    fixed url

--- route/medicalRecordRoute.js
+++ route/medicalRecordRoute.js
@@ -10,7 +10,7 @@ router.get("/getMyRecords", protect("patient", "doctor", "admin", "superadmin", 
 router.patch("/updateMedicalRecord/:id", protect("patient", "doctor", "admin", "superadmin", "subadmin","serviceprovider"), medicalRecordController.updateMedicalRecord);
 router.post("/shareMedicalRecord/:id/share", protect("patient", "doctor", "admin", "superadmin", "subadmin","serviceprovider"), medicalRecordController.shareMedicalRecord);
 router.delete("/deleteMedicalRecord/:id", protect("patient", "doctor", "admin", "superadmin", "subadmin","serviceprovider"), medicalRecordController.deleteMedicalRecord);
-router.get("/appointment/:appointmentId", protect("patient", "doctor", "admin", "superadmin", "subadmin"), medicalRecordController.getRecordsByAppointmentId);
+// router.get("/appointment/:appointmentId", protect("patient", "doctor", "admin", "superadmin", "subadmin"), medicalRecordController.getRecordsByAppointmentId);
 router.get("/prescriptions/appointment/:appointmentId", protect("patient", "doctor", "admin", "superadmin", "subadmin"), medicalRecordController.getPrescriptionsByAppointmentId);
 
 module.exports = router;
